d1c0923d3abd79ebe41427f3142a1589f75239aa
AhsanSawrwar0413 <ahsansarwar0413@gmail.com>
2024-06-24 01:27:49 +0500

    Completed Static Typescript gym app with tailwind and framer motion

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }] //we are replacing ../../src to @
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
